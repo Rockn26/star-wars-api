4736a1532145378bda84560b6dfc2e6b3ca80857
Rockn26 <irfanank26@gmail.com>
2024-02-24 17:27:39 +0300

    add Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
+import Main from './pages/Main'
 
 function App() {
-
-
   return (
-    <>
-      
-    </>
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}>
+
+        </Route>
+      </Routes>
+      </BrowserRouter>
+    </div>
   )
 }
 
