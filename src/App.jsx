135f9a65c86916c79cb0761cbd04d930b8d58386
Rockn26 <irfanank26@gmail.com>
2024-02-26 17:45:15 +0300

    add Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Main from './pages/Main'
+import Main from './pages/Main/Main'
+import { useState } from 'react'
+import StarshipPage from './pages/StarshipPage'
+
 
 function App() {
+  const [selectedStarship , setSelectedStarship] = useState({})
+
   return (
     <div>
       <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />}>
-
-        </Route>
+        <Route path="/" element={<Main  setSelectedStarship={setSelectedStarship} />} />
+        <Route path={`/${selectedStarship.name}`} element={<StarshipPage starship={selectedStarship} />} />
       </Routes>
       </BrowserRouter>
     </div>
