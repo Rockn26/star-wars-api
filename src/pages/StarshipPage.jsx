c38e221fc77ad921675b3473b97b036908dbd45b
Rockn26 <irfanank26@gmail.com>
2024-02-26 17:44:29 +0300

    add Starship page

--- src/pages/StarshipPage.jsx
+++ src/pages/StarshipPage.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
-const StarshipPage = () => {
+const StarshipPage = ({starship}) => {
   return (
     <div>
+      <h1>{starship.name}</h1>
+      <h4>Model: {starship.model}</h4>
+      <h4>Hyperdrive Rating : {starship.hyperdrive_rating}</h4>
+      <h4>Passengers: {starship.passengers}</h4>
+      <h4>Max Atmosphering Speed: {starship.max_atmosphering_speed}</h4>
+      <h4>Crew: {starship.crew}</h4>
+      <h4>Cargo Capacity: {starship.cargo_capacity}</h4>
+
+      <Link to={"/"} >
+        return to Main Page
+      </Link>
       
+    
+  
     </div>
   )
 }
